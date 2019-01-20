559f023c70ccdb3d8cb4ccc96052aee25e44b492
Anil <fejitj3n@yahoo.com>
2019-01-20 19:00:51 +0300

    .

--- lib.go
+++ lib.go
@@ -1,6 +1,8 @@
 package main
 
-import "errors"
+import (
+	"errors"
+)
 
 type Vector []float64
 
@@ -103,6 +105,50 @@ func (m Matrix) Add(m2 Matrix) (Matrix, error) {
 	return newMatrix, nil
 }
 
+// Negative x
+func (m Matrix) Negative() Matrix {
+	var newMatrix Matrix
+
+	for _, v := range m {
+		var newVector Vector
+		for _, e := range v {
+			newVector = append(newVector, e*-1)
+		}
+		newMatrix = append(newMatrix, newVector)
+	}
+
+	return newMatrix
+}
+
+// Subtract x
+func (m Matrix) Subtract(m2 Matrix) (Matrix, error) {
+	var newMatrix Matrix
+	t := isSameDimensions(m, m2)
+	if t == false {
+		return nil, errors.New("matrices dimensions are not same")
+	}
+
+	l := m.getRowCount()
+
+	for i := 0; i < l; i++ {
+		l2 := m[i].getLen()
+		var newVector Vector
+		for j := 0; j < l2; j++ {
+			newVector = append(newVector, m[i][j]-m2[i][j])
+		}
+		newMatrix = append(newMatrix, newVector)
+	}
+
+	return newMatrix, nil
+}
+
+// Divide x
+func (m Matrix) Divide(m2 Matrix) (Matrix, error) {
+	var newMatrix Matrix
+	// need inverse for this
+	return newMatrix, nil
+}
+
 // Inverse x
 func (m Matrix) Inverse() (Matrix, error) {
 	return nil, nil
@@ -112,3 +158,83 @@ func (m Matrix) Inverse() (Matrix, error) {
 func (m Matrix) Transpose() (Matrix, error) {
 	return nil, nil
 }
+
+// GetSubMatrix x // maybe check for squareness of matrix
+func (m Matrix) GetSubMatrix() []Matrix {
+
+	var subMatrices []Matrix
+	var newMatrix Matrix
+
+	rows := m.getRowCount()
+	for z := 0; z < rows; z++ {
+		for i, v := range m {
+			var newVector Vector
+			for j := range v {
+				if i != 0 {
+					if j != z {
+						newVector = append(newVector, m[i][j])
+					}
+				}
+			}
+			if newVector.getLen() != 0 {
+				newMatrix = append(newMatrix, newVector)
+			}
+		}
+		subMatrices = append(subMatrices, newMatrix)
+		newMatrix = nil
+	}
+	return subMatrices
+}
+
+// Determinant x
+func (m Matrix) Determinant() (float64, error) {
+	s := m.IsSquare()
+	if s != true {
+		return 0, errors.New("matrix is not square")
+	}
+	if m.getRowCount() == 2 {
+		return (m[0][0] * m[1][1]) - (m[0][1] * m[1][0]), nil
+	} else if m.getRowCount() >= 3 {
+		det := 0.0
+		subMatrices := m.GetSubMatrix()
+		for i, v := range m[0] {
+			d, err := subMatrices[i].Determinant()
+			if err != nil {
+				return 0, err
+			}
+			if i%2 == 0 {
+				det += v * d
+			} else {
+				det += v * -1 * d
+			}
+		}
+		return det, nil
+	} else {
+		return 0, errors.New("unknown error")
+	}
+}
+
+// ! old 3-dim matrix det solution
+// } else if m.getRowCount() == 3 {
+// 	fPart := Matrix{
+// 		Vector{m[1][1], m[1][2]},
+// 		Vector{m[2][1], m[2][2]},
+// 	}
+// 	sPart := Matrix{
+// 		Vector{m[1][0], m[1][2]},
+// 		Vector{m[2][0], m[2][2]},
+// 	}
+// 	tPart := Matrix{
+// 		Vector{m[1][0], m[1][1]},
+// 		Vector{m[2][0], m[2][1]},
+// 	}
+
+// 	detF, _ := fPart.Determinant()
+// 	detS, _ := sPart.Determinant()
+// 	detT, _ := tPart.Determinant()
+
+// 	first := detF * m[0][0]
+// 	second := detS * m[0][1]
+// 	third := detT * m[0][2]
+
+// 	return first - second + third, nil
--- main.go
+++ main.go
@@ -4,21 +4,34 @@ import "fmt"
 
 func main() {
 	// v1 := Vector{1, 2, 3, 4, 5, 6, 7}
-	m1 := Matrix{
-		Vector{1, 2},
-		Vector{2, 4},
-		Vector{5, 1},
+	// m1 := Matrix{
+	// 	Vector{1, 2},
+	// 	Vector{2, 4},
+	// 	Vector{5, 1},
+	// }
+
+	// m2 := Matrix{
+	// 	Vector{1, 2, -3, 0},
+	// 	Vector{2, 4, 1, 0},
+	// 	Vector{-5, 1, 2, 0},
+	// 	Vector{6, 12, 0, -5},
+	// }
+
+	m3 := Matrix{
+		Vector{5, -2, 22},
+		Vector{8, 89, 5},
+		Vector{66, 112, 52},
 	}
 
-	m2 := Matrix{
-		Vector{1, 2},
-		Vector{2, 4},
-		Vector{5, 1},
-	}
+	// a, err := m1.Add(m2)
+	// checker(err)
+	// fmt.Println(a)
 
-	fmt.Println(m1)
-	fmt.Println(m2)
-	a, err := m1.Add(m2)
+	// fmt.Println(m1.Negative())
+	// fmt.Println(m2.Negative())
+	fmt.Println(m3)
+	fmt.Println()
+	a, err := m3.Determinant()
 	checker(err)
 	fmt.Println(a)
 }

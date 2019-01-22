68eff1545a9324d1e97eb94447837d7ebbf4cf2b
Anil <fejitj3n@yahoo.com>
2019-01-22 22:33:39 +0300

    const

---
+++ physics/constants/const.go
@@ -0,0 +1,66 @@
+package constants
+
+import "math"
+
+// C speed of light in a vacuum - m/s
+var C = 299792458.0
+
+// G gravitational constant - N m^2 / kg^2
+var G = 6.67408e-11
+
+// H planck constant - J s
+var H = 6.62607015e-34
+
+// HC h c - J m
+var HC = 1.98644586e-25
+
+// Hbar - J s
+var Hbar = 1.05457182e-34
+
+// E elementary charge - C
+var E = 1.602176634e-19
+
+// E0 electric permitivitty - C^2 / N m^2
+var E0 = 8.854e-12
+
+// M0 magnetic permitivitty - T m/A
+var M0 = math.Pi * 4e-7
+
+// Na avogadro constant - 1/mol
+var Na = 6.02214076e+23
+
+// K boltzman constant - J/K
+var K = 1.380649e-23
+
+// GasConstant - J/mol K  -- R = Na k
+var GasConstant = 8.314462618
+
+// SBc stefan-boltzman constant - W/m^2 K^4
+var SBc = 5.670370374419e-8
+
+// B wien displacement constant - mm K
+var B = 2.897771955
+
+// Mu atomic mass constant - kg
+var Mu = 1.660e-27
+
+// Me electron mass - kg
+var Me = 9.109e-31
+
+// Mp proton mass - kg
+var Mp = 1.672e-27
+
+// Mn neutron mass - kg
+var Mn = 1.674e-27
+
+// Kcd luminous efficacy - lm / W
+var Kcd = 683.0
+
+// StdG standart gravity -  m / s^2
+var StdG = 9.80665
+
+// StdATM standart atmosphere - Pa
+var StdATM = 101325.0
+
+// H0 hubble constant - km / s / Mpc
+var H0 = 69.3
